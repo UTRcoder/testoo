a3b5b8f2eaca46808e1dfc5f173ae168e081f087
Utkarsh Rai <100745741+UTRcoder@users.noreply.github.com>
2025-09-24 23:50:42 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,11 @@ export default function Footer() {
     <footer className="mt-12 border-t">
       <div className="container py-8 flex flex-col md:flex-row items-center justify-between gap-4">
         <div className="flex items-center gap-3">
-          <div className="w-10 h-10 rounded-full bg-gradient-to-br from-primary-500 to-primary-700 flex items-center justify-center text-white font-bold">G</div>
+          <div className="w-12 h-12 rounded-full overflow-hidden flex items-center justify-center bg-">
+            <img src="icgs.png" alt="ICGS Logo" className="w-full h-full object-fit " />
+          </div>
           <div>
-            <div className="font-semibold">GLOCAL EDIT</div>
+            <div className="font-semibold">International Correction & Graphical Solution</div>
             <div className="text-sm text-gray-500">© {new Date().getFullYear()} All Right Reserved.</div>
           </div>
         </div>
